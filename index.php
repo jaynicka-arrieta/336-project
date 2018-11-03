590dbf2b77ed2ad0c3c5c8771bb3a6f8455288cf
Sam Laitha <slaitha@csumb.edu>
2018-11-03 08:21:24 +0000

    Adding main files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title> </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+</html>
